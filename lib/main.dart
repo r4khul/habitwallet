a2ee48c43b01093204069c4a83a6db76320b0698
r4khul <blackempire9177@gmail.com>
2026-02-08 01:45:41 +0530

    feat: add some deps to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(HabitWalletApp());
+  runApp(const ProviderScope(child: HabitWalletApp()));
 }
 
 class HabitWalletApp extends StatelessWidget {
