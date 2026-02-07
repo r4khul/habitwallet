4c09a29cef2a65f60cf9552ac7519387548e56aa
r4khul <blackempire9177@gmail.com>
2026-02-07 23:57:34 +0530

    init: project setup & cleanup

---
+++ android/app/src/main/kotlin/com/example/habitwallet/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.habitwallet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HabitWalletApp());
+}
+
+class HabitWalletApp extends StatelessWidget {
+  const HabitWalletApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
